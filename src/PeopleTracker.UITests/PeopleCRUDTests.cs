922960d9bee7b705e01244d63bf9c029a3ba2b78
Donovan <dlbm3@hotmail.com>
2016-03-21 09:28:05 -0700

    Adding middle name to schema and page #221

--- src/PeopleTracker.UITests/PeopleCRUDTests.cs
+++ src/PeopleTracker.UITests/PeopleCRUDTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿busing System;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -48,6 +48,8 @@ namespace PeopleTracker.UITests
          driver.FindElementByLinkText("Create New").Click();
          driver.FindElementById("FirstName").Clear();
          driver.FindElementById("FirstName").SendKeys(browser);
+         driver.FindElementById("MiddleName").Clear();
+         driver.FindElementById("MiddleName").SendKeys("E");
          driver.FindElementById("LastName").Clear();
          driver.FindElementById("LastName").SendKeys("User");
          driver.FindElementByCssSelector("input.btn").Click();
