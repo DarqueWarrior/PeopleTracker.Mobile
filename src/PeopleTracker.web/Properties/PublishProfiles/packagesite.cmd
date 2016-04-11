d19d453e6d2599a97464f9f30dde42a927f0300b
Donovan Brown <dlbm3@hotmail.com>
2016-04-10 19:51:43 -0700

    fixed cmd

--- src/PeopleTracker.web/Properties/PublishProfiles/packagesite.cmd
+++ src/PeopleTracker.web/Properties/PublishProfiles/packagesite.cmd
@@ -1,5 +1,5 @@
 set path=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Web Tools\External;%1\node_modules\.bin;%path%
 
-dnu.cmd publish "%1" --out "%2" --wwwroot-out "wwwroot"
+dnu.cmd publish "%1" --out "%2" --wwwroot "wwwroot" --wwwroot-out "wwwroot"
 
 exit /b 0
