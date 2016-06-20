327e445ecf33f1678a9b56e547aea11aef7321b2
Donovan Brown <dlbm3@hotmail.com>
2016-06-20 13:41:20 -0700

    Added the add middle name code for mobile.

---
+++ MiddleName_Add/src/PeopleTracker.Mobile/Models/Person.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PeopleTracker.Mobile.Models
+{
+   public partial class Person : INotifyPropertyChanged
+   {
+      private string firstName;
+      private string lastName;
+      private string middleName;
+      public int ID { get; set; }
+
+      public string FirstName
+      {
+         get { return firstName; }
+         set
+         {
+            if (firstName != value)
+            {
+               firstName = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      public string LastName
+      {
+         get { return lastName; }
+         set
+         {
+            if (lastName != value)
+            {
+               lastName = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      public string MiddleName
+      {
+         get { return middleName; }
+         set
+         {
+            if (middleName != value)
+            {
+               middleName = value;
+               OnPropertyChanged();
+            }
+         }
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+   }
+}
