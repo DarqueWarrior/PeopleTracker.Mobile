d03523f42cbb4cadbdcfb6ebc90f8d88cb369111
Donovan Brown <dlbm3@hotmail.com>
2016-06-21 15:46:32 -0700

    getting add and remove

--- MiddleName_Add/src/PeopleTracker.Web/Models/Person.cs
+++ MiddleName_Add/src/PeopleTracker.Web/Models/Person.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PeopleTracker.Web.Models
+﻿namespace PeopleTracker.Web.Models
 {
    public class Person
    {
       public int ID { get; set; }
 
-      [Display(Name = "First Name")]
       public string FirstName { get; set; }
 
-      [Display(Name = "Last Name")]
       public string LastName { get; set; }
 
-      [Display(Name = "Middle Name")]
       public string MiddleName { get; set; }
    }
 }
--- MiddleName_Remove/src/PeopleTracker.Web/Models/Person.cs
+++ MiddleName_Remove/src/PeopleTracker.Web/Models/Person.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PeopleTracker.Web.Models
+﻿namespace PeopleTracker.Web.Models
 {
    public class Person
    {
