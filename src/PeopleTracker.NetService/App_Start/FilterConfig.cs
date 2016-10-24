3a8e162b33312c8cb1f03153642260eb3d279018
Donovan Brown <dlbm3@hotmail.com>
2016-10-24 03:53:16 -0500

    Added AI

--- src/PeopleTracker.NetService/App_Start/FilterConfig.cs
+++ src/PeopleTracker.NetService/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace PeopleTracker.NetService
    {
       public static void RegisterGlobalFilters(GlobalFilterCollection filters)
       {
-         filters.Add(new HandleErrorAttribute());
+         filters.Add(new ErrorHandler.AiHandleErrorAttribute());
       }
    }
 }
